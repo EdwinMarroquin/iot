a56f8d3fd8fe86465f791447734a4246f03a4826
Edwin Marroquin <efmarroquinb@correo.udistrital.edu.co>
2022-04-01 17:16:32 -0500

    FIX GoogleMap
    Fixed request last data from thingspeak

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -9,7 +9,7 @@
   >
     <Marker v-for="(s, k) in dataStations" :key="k" :options="s.options">
       <InfoWindow>
-        <div class="ui-mark-title">{{ s.info.id }}</div>
+        <div class="ui-mark-title">{{ s.info.name }}</div>
 
         <div class="ui-mark-content">
           <div class="ui-mark-content-title">Temperatura:</div>
@@ -32,7 +32,7 @@
 </template>
 
 <script setup>
-import { onBeforeMount, ref } from "vue";
+import { onBeforeMount, onMounted, ref , reactive} from "vue";
 import { GoogleMap, Marker, InfoWindow } from "vue3-google-map";
 
 import { stations } from "@/data/stations.js";
@@ -41,19 +41,18 @@ const listStations = ref(stations);
 
 const center = ref({});
 const fullData = ref([]);
-const dataStations = ref([]);
+const dataStations = reactive([]);
 
-const getData = async () => {
+const getStations = async () => {
   listStations.value.map(async (station) => {
     const data = await fetch(
-      `https://api.thingspeak.com/channels/${station.id}/feeds.json`
+      `https://api.thingspeak.com/channels/${station.id}/feeds.json?results=1`
     );
     const d = await data.json();
     await fullData.value.push(d);
-    // console.log(d)
-    dataStations.value.push({
+    dataStations.push({
       info: {
-        id: d.channel.name,
+        name: d.channel.name,
         t: parseFloat(d.feeds[d.feeds.length - 1].field1),
         h: parseFloat(d.feeds[d.feeds.length - 1].field2),
       },
@@ -66,10 +65,27 @@ const getData = async () => {
       },
     });
   });
+}
+
+const getData = async () => {
+  listStations.value.map(async (station) => {
+    const data = await fetch(
+      `https://api.thingspeak.com/channels/${station.id}/feeds.json?results=1`
+    );
+    const d = await data.json();
+    fullData.value.forEach((s, i) => {
+      if(s.channel.id === d.channel.id) {
+        dataStations[i].info.t = parseFloat(d.feeds[0].field1)
+        dataStations[i].info.h = parseFloat(d.feeds[0].field2)
+      }
+    })
+  });
 };
 
-onBeforeMount(() => {
-  getData();
+onBeforeMount(async() => {
+  await getStations();
+  setInterval(() => { getData() }, 1000)
+  // getData();
   center.value = { lat: 4.6456398, lng: -74.1788349 };
 });
 </script>
