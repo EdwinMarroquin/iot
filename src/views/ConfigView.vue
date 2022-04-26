62b502324eee73134a5770d9b01918c4ed5ea478
Edwin Marrpqion <efmarroquinb@correo.udistrital.edu.co>
2022-04-25 21:11:18 -0500

    UPDATE

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -1,50 +1,54 @@
 <template>
   <div class="config">
-    <div class="display">
-      <CardSensor dummy :key="cS" />
-      <img :src="imgMap" alt="" :key="mS" />
-    </div>
-    <div>
-      <div class="config-item">
-        UNIDADES :
-        <fieldset class="units">
-          °C
-          <input
-            type="checkbox"
-            name="units"
-            id="units"
-            @input="updateUnits"
-            :checked="getCelciusChecked"
-          />
-          <label for="units"></label>
-          °F
-        </fieldset>
+    <div class="config-display">
+      <CardSensor dummy :key="cS" class="config-display-sensor" />
+      <div class="config-display-map">
+        <img :src="imgMap" alt="" :key="mS" />
       </div>
-      <div class="config-map">
-        <div class="config-map-item" v-for="(mk, mi) in layersStyles" :key="mi">
-          <input
-            type="radio"
-            name="layer"
-            :id="`sml${mi}`"
-            :value="mk"
-            @change="updateStyleMap"
-            :checked="getLayerChecked(mk)"
-          />
-          <label :for="`sml${mi}`">{{ mk.toUpperCase() }}</label>
+    </div>
+    <div class="config-options">
+      <h1 class="config-options-title">Configuración</h1>
+      <div class="config-options-option">
+        <div class="option">
+          <div class="option-title">
+            UNIDADES
+          </div>
+          <div class="option-selections">
+            <div class="option-selections-item">
+              <input type="checkbox" name="units" id="units" @input="updateUnits">
+              <label for="units"></label>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <div class="config-title">LANGUAGUE</div>
-      <div class="config-lang" v-for="(lk, li) in languagues" :key="li">
-        <input
-          type="radio"
-          name="lng"
-          :id="`lng${li}`"
-          :value="lk"
-          @change="updateLang"
-        />
-        <label :for="`lng${li}`">{{ lk.toUpperCase() }}</label>
+
+      <div>
+        <div class="config-item">
+          UNIDADES :
+          <fieldset class="units">
+            °C
+            <input type="checkbox" name="units" id="units" @input="updateUnits"
+              :checked="getCelciusChecked" />
+            <label for="units"></label>
+            °F
+          </fieldset>
+        </div>
+        <div class="config-map">
+          <div class="config-map-item" v-for="(mk, mi) in layersStyles"
+            :key="mi">
+            <input type="radio" name="layer" :id="`sml${mi}`" :value="mk"
+              @change="updateStyleMap" :checked="getLayerChecked(mk)" />
+            <label :for="`sml${mi}`">{{ mk.toUpperCase() }}</label>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="config-title">LANGUAGUE</div>
+        <div class="config-lang" v-for="(lk, li) in languagues" :key="li">
+          <input type="radio" name="lng" :id="`lng${li}`" :value="lk"
+            @change="updateLang" />
+          <label :for="`lng${li}`">{{ lk.toUpperCase() }}</label>
+        </div>
       </div>
     </div>
   </div>
@@ -96,32 +100,12 @@ const updateStyleMap = async (e) => {
 
 onBeforeMount(async () => {
   getCelciusChecked;
-  });
+});
 
 onMounted(async () => {
-   getRouteMap();
+  getRouteMap();
 });
 </script>
 
-<style>
-@import url(https://unpkg.com/leaflet@1.7.1/dist/leaflet.css);
-
-.display {
-  pointer-events: none;
-}
-
-.map {
-  display: flex;
-  flex: 1;
-  width: 100%;
-  /* height: 100%; */
-  padding-bottom: 25%;
-  border: 1px solid silver;
-  border-radius: 0.5rem;
-}
-
-.leaflet-control-zoom.leaflet-bar.leaflet-control,
-.leaflet-control-attribution.leaflet-control {
-  display: none !important;
-}
+<style lang="scss">
 </style>
