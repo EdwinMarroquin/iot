f0f4aa217bd3187cb63e0b4533a41fc33268754c
Edwin Marroquin <efmarroquinb@correo.udistrital.edu.co>
2022-05-09 13:10:19 -0500

    Update leafletMap.vue

--- src/components/leafletMap.vue
+++ src/components/leafletMap.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script setup>
-import pointGrid from '@turf/point-grid'
-import isobands from '@turf/isobands'
+import pointGrid from "@turf/point-grid";
+import isolines from "@turf/isolines";
 
 import leaflet from "leaflet";
 import { onMounted, ref } from "vue";
@@ -23,40 +23,49 @@ const props = defineProps({
   },
 });
 
+function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 const L = leaflet;
 
-const extend = ref([-74.217, 4.465, -74.003, 4.791])
+const extend = ref([-74.217, 4.465, -74.003, 4.791]);
 
-const spacing = ref(1)
+const spacing = ref(1);
 
-const grid = pointGrid(extend.value, spacing.value, {unit:"kilometers"});
+const grid = pointGrid(extend.value, spacing.value, { unit: "kilometers" });
 
 for (var i = 0; i < grid.features.length; i++) {
-  grid.features[i].properties.latitude = Math.random() * 10;
-  grid.features[i].properties.longitude = Math.random() * 10;
-  grid.features[i].properties.temperature = Math.random() * 10;
+  grid.features[i].properties.latitude = Math.random() * 1;
+  grid.features[i].properties.longitude = Math.random() * 1;
+  grid.features[i].properties.temperature = randomRange(15, 32);
 }
-const breaks = [1,215,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
 
+const breaks = Array(20)
+  .fill()
+  .map((el, i) => (i + 1)*2);
 
-const isobands_options = {
+console.log(breaks);
+
+const isolines_options = {
+  resolution: 2,
   zProperty: "temperature",
   commonProperties: {
-    "fill-opacity": 0.8
+    "fill-opacity": 0.5,
   },
   breaksProperties: [
-    {fill: "#e3e3ff"},
-    {fill: "#c6c6ff"},
-    {fill: "#a9aaff"},
-    {fill: "#8e8eff"},
-    {fill: "#7171ff"},
-    {fill: "#5554ff"},
-    {fill: "#3939ff"},
-    {fill: "#1b1cff"}
-  ]
+    { fill: "#e3e3ff" },
+    { fill: "#c6c6ff" },
+    { fill: "#a9aaff" },
+    { fill: "#8e8eff" },
+    { fill: "#7171ff" },
+    { fill: "#5554ff" },
+    { fill: "#3939ff" },
+    { fill: "#1b1cff" },
+  ],
 };
 
-const lines = isobands(grid, breaks, isobands_options);
+const lines = isolines(grid, breaks, isolines_options);
 
 onMounted(async () => {
   let optionsMap = {
@@ -70,22 +79,22 @@ onMounted(async () => {
     zoom: 11,
   });
 
-  L.tileLayer(layers[await localStorage.getItem("layername")].url, optionsMap).addTo(map);
+  L.tileLayer(
+    layers[await localStorage.getItem("layername")].url,
+    optionsMap
+  ).addTo(map);
 
   L.geoJson(lines, {
     weight: 1,
-    color: "#00f2"
+    color: "#00f2",
   }).addTo(map);
 
   if (props.geoPoints !== {} && props.geoPoints.lenght !== 0) {
     L.geoJSON(props.geoPoints, {
       weight: 3,
-      color: "#de000099"
+      color: "#de000099",
     }).addTo(map);
   }
-
-  // if (props.isoLines !== {} && props.isoLines.lenght !== 0) {
-  // }
 });
 </script>
 
@@ -95,7 +104,12 @@ onMounted(async () => {
   height: 100%;
 }
 
-#map>div.leaflet-pane.leaflet-map-pane>div.leaflet-pane.leaflet-overlay-pane>svg>g>path.leaflet-interactive {
+#map
+  > div.leaflet-pane.leaflet-map-pane
+  > div.leaflet-pane.leaflet-overlay-pane
+  > svg
+  > g
+  > path.leaflet-interactive {
   stroke-width: 1px;
   fill-opacity: 0.1;
 }
